a1efbd461a185757cae03fb31ec64609210ef36b
nadhif1867 <0106.nadhif@gmail.com>
2024-09-26 20:21:56 +0700

    js5 - praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,10 +33,17 @@ Route::get('/user', [UserController::class, 'index']);
 // Route untuk Tambah
 Route::get("/user/tambah", [UserController::class, 'tambah']);
 
+// Route untuk tambah_simpan
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 
+// Route untuk ubah{id}
 Route::get("/user/ubah/{id}", [UserController::class, 'ubah']);
 
+// Route untuk ubah_simpan{id}
 Route::put("/user/ubah_simpan/{id}", [UserController::class, 'ubah_simpan']);
 
+// Route untuk hapus{id}
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+// Route untuk WelcomeController
+Route::get('/', [WelcomeController::class, 'index']);
