dd342cafcc91ac766f6e33b87f73f87c874ab524
nadhif1867 <0106.nadhif@gmail.com>
2024-10-02 02:59:54 +0700

    js5 - tugas_praktikum

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -9,9 +9,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class LevelModel extends Model
 {
     use HasFactory;
-
+    
     protected $table = 'm_level'; // Mendefinisikan nama tabel
     protected $primaryKey = 'level_id'; // Mendefinisikan primary key
+
+    protected $fillable = ['level_kode', 'level_nama'];
 
     // Relasi ke UserModel (hasMany)
     public function users(): HasMany {
