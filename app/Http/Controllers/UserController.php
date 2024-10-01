12897fa2d07afbfc71361901b93ca4bb7ca11baf
nadhif1867 <0106.nadhif@gmail.com>
2024-10-01 07:47:37 +0700

    js5 - praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,6 +31,7 @@ class UserController extends Controller
     public function list(Request $request){
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
 
+        
         //Filter data user berdasarkan level_id
         if($request->level_id){
             $users->where('level_id', $request->level_id);
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,6 +12,7 @@
             @if(session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
             @endif
+            
             @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
