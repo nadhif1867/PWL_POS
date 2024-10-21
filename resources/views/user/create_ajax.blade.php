eca929a27edc1ba7028158d00ca04f96a3e35a46
nadhif1867 <0106.nadhif@gmail.com>
2024-10-21 15:47:09 +0700

    #

--- resources/views/user/create_ajax.blade.php
+++ resources/views/user/create_ajax.blade.php
@@ -1,45 +1,45 @@
 <form action="{{ url('/user/ajax') }}" method="POST" id="form-tambah">
-    @csrf
-    <div id="modal-master" class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">&times;</span></button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label>Level Pengguna</label>
-                    <select name="level_id" id="level_id" class="form-control" required>
-                        <option value="">- Pilih Level -</option>
-                        @foreach ($level as $l)
+@csrf
+<div id="modal-master" class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="modal-body">
+            <div class="form-group">
+                <label>Level Pengguna</label>
+                <select name="level_id" id="level_id" class="form-control" required>
+                    <option value="">- Pilih Level -</option>
+                    @foreach ($level as $l)
                         <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
-                        @endforeach
-                    </select>
-                    <small id="error-level_id" class="error-text form-text text-danger"></small>
-                </div>
-                <div class="form-group">
-                    <label>Username</label>
-                    <input value="" type="text" name="username" id="username" class="form-control" required>
-                    <small id="error-username" class="error-text form-text text-danger"></small>
-                </div>
-                <div class="form-group">
-                    <label>Nama</label>
-                    <input value="" type="text" name="nama" id="nama" class="form-control" required>
-                    <small id="error-nama" class="error-text form-text text-danger"></small>
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input value="" type="password" name="password" id="password" class="form-control" required>
-                    <small id="error-password" class="error-text form-text text-danger"></small>
-                </div>
+                    @endforeach
+                </select>
+                <small id="error-level_id" class="error-text form-text text-danger"></small>
             </div>
-            <div class="modal-footer">
-                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
-                <button type="submit" class="btn btn-primary">Simpan</button>
+            <div class="form-group">
+                <label>Username</label>
+                <input value="" type="text" name="username" id="username" class="form-control" required>
+                <small id="error-username" class="error-text form-text text-danger"></small>
+            </div>
+            <div class="form-group">
+                <label>Nama</label>
+                <input value="" type="text" name="nama" id="nama" class="form-control" required>
+                <small id="error-nama" class="error-text form-text text-danger"></small>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input value="" type="password" name="password" id="password" class="form-control" required>
+                <small id="error-password" class="error-text form-text text-danger"></small>
             </div>
         </div>
+        <div class="modal-footer">
+            <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </div>
     </div>
+</div>
 </form>
 <script>
     $(document).ready(function() {
